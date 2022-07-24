2c37bef0f4d8c6f934865207636e8b168c1a657f
Shruti-student <109878992+Shruti-student@users.noreply.github.com>
2022-07-25 02:51:47 +1000

    Update GA8.py

--- GA8.py
+++ GA8.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-  st.title("Even or Odd")
+  st.title("Maximum_number")
   html_temp = """
   <div style="background-color:white;padding:12px">
   <h2 style="color:blue;text-align:center;">Streamlit maximum number App <h2>
@@ -12,7 +12,7 @@ def main():
   number2 = st.number_input("Enter second number",min_value=-1000000000000.0,max_value=1000000000000.0)
   number3 = st.number_input("Enter third number",min_value=-1000000000000.0,max_value=1000000000000.0)
   result = None
-  if st.button("Check"):
+  if st.button("Find maximum"):
     result = max(number1,number2,number3)
   st.success(f"The largest number is {result}")
 if __name__=="__main__":
