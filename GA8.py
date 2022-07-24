771852b787a06504cb38dd0fdcad1f4ed9f572c5
Shruti-student <109878992+Shruti-student@users.noreply.github.com>
2022-07-25 02:40:09 +1000

    Add files via upload

---
+++ GA8.py
@@ -0,0 +1,17 @@
+def main():
+  st.title("Even or Odd")
+  html_temp = """
+  <div style="background-color:white;padding:12px">
+  <h2 style="color:blue;text-align:center;">Streamlit maximum number App <h2>
+  </div>
+  """
+  st.markdown(html_temp,unsafe_allow_html=True)
+  number1 = st.number_input("Enter first number",min_value=-1000000000000.0,max_value=1000000000000.0)
+  number2 = st.number_input("Enter second number",min_value=-1000000000000.0,max_value=1000000000000.0)
+  number3 = st.number_input("Enter third number",min_value=-1000000000000.0,max_value=1000000000000.0)
+  result = None
+  if st.button("Check"):
+    result = max(number1,number2,number3)
+  st.success(f"The largest number is {result}")
+if __name__=="__main__":
+  main()
